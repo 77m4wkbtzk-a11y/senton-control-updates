25367e6a5a8a6ebb429b704ee5c9d9b85bb7aed8
77m4wkbtzk-a11y <77m4wkbtzk@privaterelay.appleid.com>
2026-08-27 02:46:35 +0930

    Add Senton Control v1.2.1 config

---
+++ source/config.py
@@ -0,0 +1,18 @@
+APP_NAME = "Senton Control"
+APP_VERSION = "1.2.1"
+
+# Senton Control online update manifest
+UPDATE_MANIFEST_URL = "https://raw.githubusercontent.com/77m4wkbtzk-a11y/senton-control-updates/main/update.json"
+
+PI_HOST = "192.168.1.50"
+PI_PORT = 8765
+
+TAKEOVER_DELAY_SECONDS = 10
+SESSION_LIMIT_SECONDS = 30 * 60
+
+DEMO_MAX_SPEED_KMH = 2.0
+NORMAL_AUTO_MAX_SPEED_KMH = 5.0
+
+STOP_ON_REMOTE_LOSS_UNLESS_AUTO_ARMED = True
+STOP_ON_SENSOR_FAILURE = True
+STOP_ON_CAMERA_FAILURE = True
