b44d0740434507c7aec9241b6c2e6fb479e13e33
77m4wkbtzk-a11y <77m4wkbtzk@privaterelay.appleid.com>
2026-08-27 10:35:06 +0930

    Run old-update cleanup on v1.2.7 startup

--- source/main.py
+++ source/main.py
@@ -1,11 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from dashboard import SentonDashboard
-from updater import cleanup_expired_backup
 from config import APP_NAME, APP_VERSION
+from cleanup import cleanup_obsolete_update_artifacts
 
 if __name__ == "__main__":
-    cleanup_expired_backup()
+    cleanup_obsolete_update_artifacts()
     app = QApplication(sys.argv)
     app.setApplicationName(APP_NAME)
     window = SentonDashboard(APP_VERSION)
